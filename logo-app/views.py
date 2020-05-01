0d8f0b55ea9cdba216f3b0a4707f83be5cc7ebc0
luabud <luabud@microsoft.com>
2020-05-01 14:22:27 -0700

    rename logo-app to logoapp and fixing path to mask

--- logo-app/views.py
+++
@@ -1,8 +0,0 @@
-from . import app
-
-@app.route('/')
-def index():
-    return 'Hello World!'
-    # output = generate_fig("https://code.visualstudio.com/docs/python/python-tutorial")
-    # return Response(output.getvalue(), mimetype="image/png")
-    
