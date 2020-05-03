ea1ff8725d760bdb6cb8cdbd8bb03695d503561c
luabud <luabud@microsoft.com>
2020-05-02 18:58:50 -0700

    Fixing generate_logo

--- generate_logo.py
+++ generate_logo.py
@@ -1,40 +1,65 @@
-from bs4 import BeautifulSoup as bs
-from wordcloud import WordCloud, STOPWORDS, ImageColorGenerator
-import numpy as np
-from PIL import Image
-import matplotlib.pyplot as plt
+import base64
 import io
-import requests
+import pathlib
+from os import path
 
-target_url = "https://code.visualstudio.com/docs/python/python-tutorial"
-mask = "images/python-colored-mask.png"
+import httpx
+import matplotlib.pyplot as plt
+import numpy as np
+from bs4 import BeautifulSoup as bs
+from PIL import Image
+from wordcloud import STOPWORDS, ImageColorGenerator, WordCloud
 
 
-def generate_fig(target_url, mask):
-    r = requests.get(url=target_url)
+def generate_fig(url, mask_path):
+    logo_path = mask_path.parent / "logo.png"
+    if not logo_path.exists():
+
+        content = parse_content(url)
+        wc_mask = np.array(Image.open(mask_path))
+        wc = generate_wordcloud(content, wc_mask)
+        generate_image(logo_path, wc, wc_mask)
+
+    return "/static/images/logo.png"
+
+
+def parse_content(url):
+    r = httpx.get(url)
     parsed_content = bs(r.content, features="html.parser")
     clean_raw_content = "".join(parsed_content.findAll(text=True))
+    return clean_raw_content
 
-    stopwords = set(STOPWORDS)
-    stopwords.update(["see", "use", "using", "tutorial", "Node", "js", "file"])
 
-    python_mask = np.array(Image.open(mask))
-
+def generate_wordcloud(content, mask=None):
+    stopwords = STOPWORDS | {
+        "see",
+        "use",
+        "using",
+        "tutorial",
+        "Node",
+        "js",
+        "file",
+    }
+    
     wc = WordCloud(
         background_color="black",
         max_words=2000,
-        mask=python_mask,
+        mask=mask,
         contour_width=10,
         contour_color="white",
         stopwords=stopwords,
-    ).generate(clean_raw_content)
+    )
 
-    image_colors = ImageColorGenerator(python_mask)
+    return wc.generate(content)
+
+
+def generate_image(logo_path, wc, mask=None):
     fig, axes = plt.subplots(1, 1)
-    axes.imshow(wc.recolor(color_func=image_colors), interpolation="bilinear")
     plt.axis("off")
+    if not mask:
+        image_colors = ImageColorGenerator(mask)
+        wc = wc.recolor(color_func=image_colors)
 
-    bytes_image = io.BytesIO()
-    plt.savefig(bytes_image, format="png", facecolor="black")
-    return bytes_image
+    axes.imshow(wc, interpolation="bilinear")
 
+    plt.savefig(logo_path, format="png", facecolor="black")
