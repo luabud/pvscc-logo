d99940ee97cc906b018269ba354d30a8e431841e
luabud <luabud@microsoft.com>
2020-05-05 18:32:47 -0700

    Fixing tests

--- tests.py
+++ tests.py
@@ -1,8 +1,14 @@
 import pytest
 import unittest
-import generate_logo
+from logo_app import generate_logo
 import io
+import pathlib
 
 class TestFigure(unittest.TestCase):
     def test_generate_figure(self):
-        self.assertTrue(isinstance(generate_logo.generate_fig(), io.BytesIO))
+        url = "https://code.visualstudio.com/docs/python/python-tutorial"
+        mask_path = (
+            pathlib.Path(__file__).parent / "logo_app" / "static" / "images" / "python-colored-mask.png"
+        )
+        # this tests fails on purpose. To fix it, replace io.BytesIO with str in the line below
+        self.assertTrue(isinstance(generate_logo.generate_fig(url,mask_path), io.BytesIO))
