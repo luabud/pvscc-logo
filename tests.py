ad354e9fb1d4b5dd496d1e5afecd5d778c4984ad
luabud <luabud@microsoft.com>
2021-03-19 17:42:19 -0700

    Make test fail again

--- tests.py
+++ tests.py
@@ -10,4 +10,4 @@ class TestFigure(unittest.TestCase):
         mask_path = (
             pathlib.Path(__file__).parent / "logo_app" / "static" / "images" / "python-colored-mask.png"
         )
-        self.assertTrue(isinstance(generate_logo.generate_fig(url,mask_path), str))
+        self.assertTrue(isinstance(generate_logo.generate_fig(url,mask_path), io.BytesIO))
