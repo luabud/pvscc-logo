9db3269775cef2066d789c050d4db24eea326eef
luabud <luabud@microsoft.com>
2021-05-12 13:26:37 -0700

    fix url

--- logo_app/generate_logo.py
+++ logo_app/generate_logo.py
@@ -13,7 +13,7 @@ from wordcloud import STOPWORDS, ImageColorGenerator, WordCloud
 def generate_fig(url, mask_path):
     logo_path = mask_path.parent / "logo.png"
     if not logo_path.exists():
-        parsed_content = parse_content("https://code.visualstudio.com/docs/python/editing")
+        parsed_content = parse_content(url)
         wc = generate_wordcloud(parsed_content,np.array(Image.open(mask_path)))
         generate_image(logo_path, wc, np.array(Image.open(mask_path)))
 
