7a531e4ad96d05df4a7c767887de4c279ac08bf5
luabud <luabud@microsoft.com>
2022-02-02 07:12:13 -0800

    fix path

--- logo_app/generate_logo_app.py
+++ logo_app/generate_logo_app.py
@@ -11,7 +11,7 @@ from wordcloud import STOPWORDS, ImageColorGenerator, WordCloud
 def generate_fig(url, mask_path):
     logo_path = mask_path.parent / "logo.png"
     if not logo_path.exists():
-        parsed_content = "" #update here to call parse_content function below
+        parsed_content = parse_content(url) #update here to call parse_content function below
         wc = generate_wordcloud(parsed_content, np.array(Image.open(mask_path)))
 
         generate_image(logo_path, wc, np.array(Image.open(mask_path)))
@@ -62,5 +62,5 @@ def generate_image(logo_path, wc, mask=None):
 
 if __name__ == "__main__":
     url = "https://code.visualstudio.com/docs/python/editing"
-    mask_path = pathlib.Path(__file__).parent / "images" / "python-colored-mask.png"
+    mask_path = pathlib.Path(__file__).parent / "logo_app" / "static" / "images" / "python-colored-mask.png"
     generate_fig("https://code.visualstudio.com/docs/python/python-tutorial", mask_path)
