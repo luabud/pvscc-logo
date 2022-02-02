c4145dfa275b0d2af6ace9cf16b0a8c9734ae86e
luabud <luabud@microsoft.com>
2022-02-02 07:08:55 -0800

    update code

--- logo_app/generate_logo.py
+++ logo_app/generate_logo.py
@@ -11,10 +11,9 @@ from wordcloud import STOPWORDS, ImageColorGenerator, WordCloud
 def generate_fig(url, mask_path):
     logo_path = mask_path.parent / "logo.png"
     if not logo_path.exists():
-        parsed_content = parse_content(
-            "https://code.visualstudio.com/docs/python/editing"
-        )
+        parsed_content = parse_content(url)
         wc = generate_wordcloud(parsed_content, np.array(Image.open(mask_path)))
+
         generate_image(logo_path, wc, np.array(Image.open(mask_path)))
 
     return "/static/images/logo.png"
@@ -52,12 +51,12 @@ def generate_wordcloud(content, mask=None):
 def generate_image(logo_path, wc, mask=None):
     fig, axes = plt.subplots(1, 1)
     plt.axis("off")
+
     if mask is not None:
         image_colors = ImageColorGenerator(mask)
         wc = wc.recolor(color_func=image_colors)
 
     axes.imshow(wc, interpolation="bilinear")
-
     plt.savefig(logo_path, format="png", facecolor="black")
 
 
